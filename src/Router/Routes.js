d9c36e03c0e44843b4b222b1aad52ce0d86e99d9
SALEEM <87938904+Saleem95@users.noreply.github.com>
2024-02-13 11:54:36 +0530

    Update Routes.js

--- src/Router/Routes.js
+++ src/Router/Routes.js
@@ -3,15 +3,11 @@ import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 import { Navbar, Container } from 'react-bootstrap';
 import Sidebar from '../Components/NavSidebar';
 import Home from '../Pages/Home';
-// import TablePage from '../Components/Table';
 
 const Routing = () => {
   return (
     <Router>
       <Container fluid>
-        {/* <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="/">React Sidebar Example</Navbar.Brand>
-        </Navbar> */}
         <Container fluid className="mt-3">
           <div className="d-inline-flex">
             <div className="sidebar">
@@ -20,7 +16,6 @@ const Routing = () => {
             <div className="container">
               <Routes>
                 <Route path="/" exact element={<Home />} />
-                {/* <Route path="/table" element={<TablePage />} /> */}
               </Routes>
             </div>
           </div>
